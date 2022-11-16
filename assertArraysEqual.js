7066241518b1c05c8e01fb17359ab7cf2ad80b62
Awo Moussa <amoussab@ualberta.ca>
2022-11-16 06:51:37 +0000

    Implemented assertArrayEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,13 @@
 
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
 const eqArrays = function (array1, array2) {
   if (array1.length === array2.length) {
     for (var i = 0; i < array2.length; i++) {
@@ -24,7 +33,7 @@ const assertArraysEqual = function (a, b) {
 };
 
 // // TEST CODE
-// assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]), false); // => false
-// assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]), true);// => true)
-// assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]), true);// => true)
+assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false); // => false
