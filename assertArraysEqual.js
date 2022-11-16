ed2aa56c652486cbcab6c446fab47737f8bdf0b4
Awo Moussa <amoussab@ualberta.ca>
2022-11-16 06:52:16 +0000

    Implemented assertArrayEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -33,7 +33,7 @@ const assertArraysEqual = function (a, b) {
 };
 
 // // TEST CODE
-assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]), true);// => true)
-assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false); // => false
+// assertEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(assertArraysEqual([1, 2, 3], [3, 2, 1]), false); // => false
+// assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]), true);// => true)
+// assertEqual(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]), false); // => false
