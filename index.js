769bd36b303303332a39dbdf253846fc22d4dbd1
Awo Moussa <amoussab@ualberta.ca>
2022-11-22 06:36:46 +0000

    Exporting all functions

---
+++ index.js
@@ -0,0 +1,40 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+
+module.exports = {
+  head: head,
+  tail: tail,
+  middle: middle,
+  countLetters: countLetters,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+  assertArraysEqual: assertArraysEqual, 
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects
+
+};
+
+
+
