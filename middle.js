6f084a860f7ce3c990250d3dcfda0728d6958f39
Awo Moussa <amoussab@ualberta.ca>
2022-11-22 05:15:53 +0000

    added module.exports at end of file

--- middle.js
+++ middle.js
@@ -1,26 +1,5 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length === array2.length) {
-    for (var i = 0; i < array2.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  else {
-    return false;
-  }
-  return true;
-
-}
-
-const assertArraysEqual = function (a, b) {
-  if (!eqArrays(a, b)) {
-      console.log(`🛑  Assertion Failed:[${a}] !== [${b}]`);
-  } else {
-      console.log(`✅  Assertion Passed:[${a}] === [${b}]`);
-  }
-};
-
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function (array) {
   let result = [];
@@ -35,5 +14,4 @@ const middle = function (array) {
 }
 
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]) // => [3, 4]
+module.exports = middle;
